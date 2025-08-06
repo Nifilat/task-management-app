15069f77398d9a53c0f4a714ffc787c0b1160584
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-08-06 17:50:43 +0100

    feat: add StatsCard component to home page and adjust layout for full screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from '@/components/navbar';
+import StatsCard from '@/components/statsCard';
 
 export default function Home() {
   return (
-    <div className="font-sans">
+    <div className="font-sans min-h-screen">
       <Navbar />
+      <StatsCard />
     </div>
   );
 }
