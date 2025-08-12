9a717c37a07f2dd67ec9958f4fbc2c905fda8275
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-08-12 13:26:37 +0100

    refactor: update middleware configuration in Redux store to improve serializable checks and enhance state management

--- store/store.ts
+++ store/store.ts
@@ -1,20 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
-import tasksReducer from '../lib/features/tasks/tasksSlice';
-import filtersReducer from '../lib/features/filters/filtersSlice';
+import { configureStore } from "@reduxjs/toolkit"
+import tasksReducer from '../lib/features/tasks/tasksSlice'
+import filtersReducer from "../lib/features/filters/filtersSlice"
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
     filters: filtersReducer,
   },
-  middleware: getDefaultMiddleware =>
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ['tasks/fetchTasks/fulfilled', 'tasks/addTask/fulfilled'],
-        ignoredPaths: ['tasks.tasks.createdAt'],
+        ignoredPaths: ["tasks.tasks", "tasks.selectedTask.createdAt"],
+        ignoredActionsPaths: ["payload.createdAt", "payload.updatedAt", "meta.arg.createdAt"],
       },
     }),
-});
+})
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
