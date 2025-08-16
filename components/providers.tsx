674c8833d7307b6d27fa79bdb5de6fbc45030a6f
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-08-16 15:25:50 +0100

    refactor: remove Toaster from RootLayout and integrate it into AppProviders for better component structure and initialization of authentication listener

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,23 +1,29 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from '@/store/store';
-import { ThemeProvider } from '@/components/theme-provider';
-import AuthProvider from '@/contexts/AuthContext';
+import { useEffect } from "react";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "sonner";
+import { store } from "@/store/store";
+import { listenToAuth } from "@/store/authSlice";
+import { useAppDispatch } from "@/hooks";
 
-interface AppProvidersProps {
-  children: React.ReactNode;
+function InitAuth() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(listenToAuth());
+  }, [dispatch]);
+  return null;
 }
 
-const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
+export default function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <AuthProvider>{children}</AuthProvider>
+        <InitAuth />
+        {children}
+        <Toaster />
       </ThemeProvider>
     </Provider>
   );
-};
-
-export default AppProviders;
+}
