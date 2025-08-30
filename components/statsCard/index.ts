150671f8515ef3993fbcedae451a80b3b559714b
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-08-30 14:03:15 +0100

    feat: create index file for stats card components and types

---
+++ components/statsCard/index.ts
@@ -0,0 +1,13 @@
+export { default as StatsCard } from './StatsCardContainer';
+export { default as SingleStatsCard } from './SingleStatsCard';
+export { calculateTaskStats, isCardActive, getCardFilterConfig } from './utils';
+
+export type {
+  StatsCardProps,
+  InteractiveStatsCardProps,
+  StatsCardContainerProps,
+  StatsCardType,
+  FilterState,
+} from './types';
+
+export { default } from './StatsCardContainer';
